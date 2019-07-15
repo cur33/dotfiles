497b8c2720dc66ea0096b9cbff81e3064186338d
Collin Rapp <collin_rapp@baylor.edu>
2019-07-15 13:12:53 -0500

    Updated windows setup script

--- setup.cmd
+++ setup.cmd
@@ -1,2 +1,18 @@
-xcopy /Y /E /I .vim H:\vimfiles
-copy /Y .vim\.vimrc H:\_vimrc
+REM xcopy /Y /E /I .vim H:\vimfiles
+REM copy /Y .vim\.vimrc H:\_vimrc
+
+SET root=%HOMEPATH%
+SET vimfiles=%root%\vimfiles
+SET vimrc=%root%\_vimrc
+
+IF NOT EXIST %vimfiles% (
+    mklink %vimfiles% .vim
+) ELSE (
+    ECHO vimfiles already exists
+)
+
+IF NOT EXIST %vimrc% (
+    mklink %vimrc% %vimfiles%\.vimrc
+) ELSE (
+    ECHO vimrc already exists
+)
